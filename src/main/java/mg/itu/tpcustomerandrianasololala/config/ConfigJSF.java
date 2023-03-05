2c2240abb8585d2b1efd32ef1aa01d0c90f30c0b
Kevin Andrianasolo <kevin.andrianasolo.lala@gmail.com>
2023-03-05 15:18:42 +0300

    Configuration JSF

---
+++ src/main/java/mg/itu/tpcustomerandrianasololala/config/ConfigJSF.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tpcustomerandrianasololala.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author kevin
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
